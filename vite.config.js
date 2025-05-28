0e34e9172d5777f406c499021a1082d72dd8a331
Shaik Shaheeba <n190470@rguktn.ac.in>
2025-05-28 19:54:12 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Resume_Portafolio',
+  base: '/Resume_Portafolio/',
   plugins: [react()],
 })
