638e3428a50bd2ce6ebc28f4d0b8fc6b7a2f269b
Shaik Shaheeba <n190470@rguktn.ac.in>
2025-05-29 11:44:08 +0530

    Fix: Set correct base path for Render

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Resume_Portofolia/'
-  ,
+  base: './',
   plugins: [react()],
 })
+
+{/*export default defineConfig({
+  base: './', // ✅ Use relative path so assets load from root
+  plugins: [react()],
+}) */}
