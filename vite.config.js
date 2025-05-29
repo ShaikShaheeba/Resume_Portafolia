32520edb9b482e7b577aba9bc185fceb304d5f54
Shaik Shaheeba <n190470@rguktn.ac.in>
2025-05-29 11:36:44 +0530

    Fix: Clean install to resolve build issues

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Resume_Portofolio/',
+  base: '/Resume_Portofolia/'
+  ,
   plugins: [react()],
 })
